95e7b380524cce83729047daa302965063ddd401
tomflynn199 <tomflynn1999@hotmail.com>
2018-06-07 12:35:38 -0400

    Twenty Third commit

--- app.py
+++ app.py
@@ -18,9 +18,37 @@ def interests():
     return render_template('Interests.html')
 
 
-@app.route('/compound')
+@app.route('/compound', methods=['GET', 'POST'])
 def compound():
     return render_template('form.html')
+
+
+@app.route('/compound', methods=['GET', 'POST'])
+def compound_interest(principal, rate, time, amount):
+
+    """
+    Computes Compound Interest
+    :param principal:
+    :param rate:
+    :param time:
+    :param amount:
+    :return:
+    """
+    formula = principal*((1.0 + (rate/100)/amount)**(time*amount))  # compound interest formula
+    return formula
+
+
+def calculate_compound_interest(p: int, i: float, t: int, n: int):
+    """
+    Calculate compound interest.
+
+    :param p: The principal investment amount.
+    :param i: The annual interest rate (decimal form).
+    :param t: Number of years the money is invested for.
+    :param n: Number of times that interest is compounded per year.
+    :return: The future value of the investment.
+    """
+    return p * ((1 + ((i / 100) / t)) ** (n * t))
 
 
 if __name__ == '__main__':
